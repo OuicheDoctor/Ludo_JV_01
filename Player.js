995cd5958d3bb7d87b812372d7b74c88ab695b61
Aurélien Panhaleux <aphax91@gmail.com>
2016-11-19 21:41:13 +0100

    Extract Player Behaviour class

---
+++ Player.js
@@ -0,0 +1,67 @@
+Player = function (phaser) {
+  Phaser.Sprite.call(this, phaser.game, 200, 200, 'player');
+
+  this.phaser = phaser;
+  this.facing = 'left';
+  this.animations.add('left', [0, 1, 2, 3], 10, true);
+  this.animations.add('turn', [4], 20, true);
+  this.animations.add('right', [5, 6, 7, 8], 10, true);
+
+  //  Enable if for physics. This creates a default rectangular body.
+  phaser.game.physics.p2.enable(this);
+
+  // Faire en sorte que le perso garde les pieds sur terre
+  this.body.fixedRotation = true;
+  // Facteur d'amortissement (perte de velocité / seconde)
+  // Ici 0.5 veut dire qu'on perd la moitié de la vélocité / seconde
+  this.body.damping = 0.5;
+
+  // See addShape, removeShape, clearShapes to add extra shapes around the Body.
+  phaser.game.world.add(this);
+};
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function () {
+  var cursors = this.phaser.cursors;
+  if (cursors.left.isDown) {
+    this.body.moveLeft(200);
+    if (this.facing != 'left') {
+      this.animations.play('left');
+      this.facing = 'left';
+    }
+  }
+  else if (cursors.right.isDown) {
+    this.body.moveRight(200);
+
+    if (this.facing != 'right') {
+      this.animations.play('right');
+      this.facing = 'right';
+    }
+  }
+  else {
+    this.body.velocity.x = 0;
+
+    if (this.facing != 'idle') {
+      this.animations.stop();
+
+      if (this.facing == 'left') {
+        this.frame = 0;
+      }
+      else {
+        this.frame = 5;
+      }
+
+      this.facing = 'idle';
+    }
+  }
+
+  if (this.phaser.jumpButton.isDown && this.canJump()) {
+    this.body.moveUp(300);
+  }
+};
+
+Player.prototype.canJump = function () {
+    return true;
+};
--- game.js
+++ game.js
@@ -1,13 +1,11 @@
+var game;
+
 window.onload = function () {
-  var game = new Phaser.Game(1366, 768, Phaser.AUTO, 'game');
+  game = new Phaser.Game(1366, 768, Phaser.AUTO, 'game');
 
   var PhaserGame = function () {
-    // init
     this.sprite = null;
-
     this.player = null;
-    this.facing = 'left';
-    this.jumpTimer = 0;
     this.cursors = null;
     this.jumpButton = null;
     this.yAxis = p2.vec2.fromValues(0, 1);
@@ -21,7 +19,7 @@ window.onload = function () {
       // this.load.baseURL = '';
       this.load.image('atari', 'assets/block.png');
       this.load.image('background', 'assets/background2.png');
-      this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+      this.load.spritesheet('player', 'assets/dude.png', 32, 48);
     },
 
     create: function () {
@@ -29,22 +27,12 @@ window.onload = function () {
 
       //  Enable p2 physics
       this.physics.startSystem(Phaser.Physics.P2JS);
-
       this.physics.p2.gravity.y = 350;
       this.physics.p2.world.defaultContactMaterial.friction = 0.3;
       this.physics.p2.world.setGlobalStiffness(1e5);
 
       //  Add a sprite
-      this.player = this.add.sprite(200, 200, 'dude');
-      this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-      this.player.animations.add('turn', [4], 20, true);
-      this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-      //  Enable if for physics. This creates a default rectangular body.
-      this.physics.p2.enable(this.player);
-
-      this.player.body.fixedRotation = true;
-      this.player.body.damping = 0.5;
+      this.player = new Player(this);
 
       var spriteMaterial = this.physics.p2.createMaterial('spriteMaterial', this.player.body);
       var worldMaterial = this.physics.p2.createMaterial('worldMaterial');
@@ -85,86 +73,28 @@ window.onload = function () {
     },
 
     update: function () {
-      if (this.cursors.left.isDown) {
-        this.player.body.moveLeft(200);
-
-        if (this.facing != 'left') {
-          this.player.animations.play('left');
-          this.facing = 'left';
-        }
-      }
-      else if (this.cursors.right.isDown) {
-        this.player.body.moveRight(200);
-
-        if (this.facing != 'right') {
-          this.player.animations.play('right');
-          this.facing = 'right';
-        }
-      }
-      else {
-        this.player.body.velocity.x = 0;
-
-        if (this.facing != 'idle') {
-          this.player.animations.stop();
-
-          if (this.facing == 'left') {
-            this.player.frame = 0;
-          }
-          else {
-            this.player.frame = 5;
-          }
-
-          this.facing = 'idle';
-        }
-      }
-
-      if (this.jumpButton.isDown && game.time.now > this.jumpTimer && this.checkIfCanJump()) {
-        this.player.body.moveUp(300);
-        this.jumpTimer = game.time.now + 750;
-      }
-
-
-    },
-
-    checkIfCanJump: function () {
-      var result = false;
-
-      for (var i = 0; i < this.physics.p2.world.narrowphase.contactEquations.length; i++) {
-        var c = this.physics.p2.world.narrowphase.contactEquations[i];
-
-        if (c.bodyA === this.player.body.data || c.bodyB === this.player.body.data) {
-          var d = p2.vec2.dot(c.normalA, this.yAxis);
-
-          if (c.bodyA === this.player.body.data) {
-            d *= -1;
-          }
-
-          if (d > 0.5) {
-            result = true;
-          }
-        }
-      }
-      return result;
     }
-  };
 
-  CustomSprite = function (game, x, y, key, group) {
-
-    Phaser.Sprite.call(this, game, x, y, key);
-
-    this.physics.p2.enable(this);
-
-    // this.anchor.x = 0.5;
-
-    // this.body.customSeparateX = true;
-    // this.body.customSeparateY = true;
-    // this.body.allowGravity = false;
-    // this.body.immovable = true;
-  };
-
-  CustomSprite.prototype = Object.create(Phaser.Sprite.prototype);
-  CustomSprite.prototype.constructor = CustomSprite;
-  CustomSprite.prototype.someBehavior = function () {
+    // checkIfCanJumpWTF: function () {
+    //   var result = false;
+    //
+    //   for (var i = 0; i < this.physics.p2.world.narrowphase.contactEquations.length; i++) {
+    //     var c = this.physics.p2.world.narrowphase.contactEquations[i];
+    //
+    //     if (c.bodyA === this.player.body.data || c.bodyB === this.player.body.data) {
+    //       var d = p2.vec2.dot(c.normalA, this.yAxis);
+    //
+    //       if (c.bodyA === this.player.body.data) {
+    //         d *= -1;
+    //       }
+    //
+    //       if (d > 0.5) {
+    //         result = true;
+    //       }
+    //     }
+    //   }
+    //   return result;
+    // }
   };
 
   game.state.add('Game', PhaserGame, true);
