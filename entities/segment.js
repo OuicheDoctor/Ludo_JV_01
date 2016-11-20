76da2ffcb615a0a89a61a930551f3d9a1322ca63
OuichesDoctor <junkerlucas@gmail.com>
2016-11-20 01:06:56 +0100

    Add segment building

--- entities/segment.js
+++ entities/segment.js
@@ -1,15 +1,32 @@
-var Segment = function (bkg, y = 0) {
+var Segment = function (id, bkg, y = 0) {
     // init
+    this.id = id;
     this.bkg = bkg;
+    this.x = null;
     this.y = y;
   };
 
   Segment.prototype = {
-    addToGame : function(game, index) {
-        var x = 0
-        if(index > 0)
-            x = game.world.width 
-        var bkgSprite = game.add.sprite(x, this.y, this.bkg);
-        game.world.resize(x + bkgSprite.width, Math.max(game.world.height, bkgSprite.height));
+    addToGame : function(instance, index) {
+      var x = 0;
+      var game = instance.game;
+      if(index > 0)
+          x = game.world.width
+      this.x = x;
+      var bkgSprite = game.add.sprite(x, this.y, this.bkg);
+      this.buildContent(instance);
+      game.world.resize(x + bkgSprite.width, Math.max(game.world.height, bkgSprite.height));
+    },
+
+    buildContent : function(instance) {
+      var data = instance.cache.getJSON("segment_" + this.id + "_data");
+
+      // Load obstacles
+      var current;
+      var segment = this;
+      data.Obstacles.forEach(function (seg) {
+          current = instance.obstacleList[seg.ID];
+          current.deploy(instance, segment.x + seg.X, segment.y + seg.Y);
+      });
     }
   };
