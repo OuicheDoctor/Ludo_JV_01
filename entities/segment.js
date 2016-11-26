8d319d06392a123aae6611e9b6740ac2bb24052b
OuichesDoctor <junkerlucas@gmail.com>
2016-11-26 11:28:27 +0100

    Start rework on segment

--- entities/segment.js
+++ entities/segment.js
@@ -1,32 +1,47 @@
-var Segment = function (id, bkg, y = 0) {
-    // init
-    this.id = id;
-    this.bkg = bkg;
-    this.x = null;
-    this.y = y;
-  };
+var Segment = function (id, bkg) {
+  // init
+  this.id = id;
+  this.bkg = bkg;
+  this.phaser = undefined;
+};
 
-  Segment.prototype = {
-    addToGame : function(instance, index) {
-      var x = 0;
-      var game = instance.game;
-      if(index > 0)
-          x = game.world.width
-      this.x = x;
-      var bkgSprite = game.add.sprite(x, this.y, this.bkg);
-      this.buildContent(instance);
-      game.world.resize(x + bkgSprite.width, Math.max(game.world.height, bkgSprite.height));
-    },
+Segment.prototype = Object.create(Phaser.Sprite.prototype);
 
-    buildContent : function(instance) {
-      var data = instance.cache.getJSON("segment_" + this.id + "_data");
+Segment.prototype.addToGame = function(instance, first = false) {
+  var x = 0;
+  var game = instance.game;
+  if(!first)
+      x = game.world.width
+  Phaser.Sprite.call(this, game, x, 0, this.bkg);
+  instance.game.world.add(this);
+  this.buildContent(instance);
+  game.world.resize(x + this.width, Math.max(game.world.height, this.height));
+  this.phaser = instance;
+};
 
-      // Load obstacles
-      var current;
-      var segment = this;
-      data.Obstacles.forEach(function (seg) {
-          current = instance.obstacleList[seg.ID];
-          current.deploy(instance, segment.x + seg.X, segment.y + seg.Y);
-      });
-    }
-  };
+Segment.prototype.buildContent = function(instance) {
+  var data = instance.cache.getJSON("segment_" + this.id + "_data");
+
+  // Load obstacles
+  var current;
+  var segment = this;
+  data.Obstacles.forEach(function (seg) {
+    current = instance.obstacleList[seg.ID];
+    current.deploy(instance, segment.x + seg.X, segment.y + seg.Y);
+  });
+
+  data.Boss.forEach(function (seg) {
+    current = instance.bossList[seg.ID];
+    current.deploy(instance, segment.x + seg.X, segment.y + seg.Y, this);
+  });
+};
+
+Segment.prototype.update = function() {
+
+  var player = this.phaser.player;
+  if((player.x > this.x && player.x < this.x + this.width)
+    && (player.y > this.y && player.y < this.y + this.height)) {
+
+  }
+
+}
